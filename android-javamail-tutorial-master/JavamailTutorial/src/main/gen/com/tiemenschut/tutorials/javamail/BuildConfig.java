99dbe5109612754a0d3ff278799ddbaac3ee7303
Krishna Patel <softeng.krishna@gmail.com>
2015-03-20 12:56:08 +0530

    Initial commit

---
+++ android-javamail-tutorial-master/JavamailTutorial/src/main/gen/com/tiemenschut/tutorials/javamail/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.tiemenschut.tutorials.javamail;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
